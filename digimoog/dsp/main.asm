764b549d88ee4c9ebf8d3c693ff2fa9efa6f7a4c
Konsta sooda Hölttä <ksooda@gmail.com>
2013-03-11 15:38:14 +0200

    add template code

---
+++ digimoog/dsp/main.asm
@@ -0,0 +1,241 @@
+;**********************************************************************
+; C H A M E L E O N   DSP Assembler file                              *
+;**********************************************************************
+; Project work template for sample-based audio I/O (polling)          *
+; Based on the example dspthru by Soundart                            *
+; Hannu Pulakka, March 2006, February 2007                            *
+; Modified by Antti Pakarinen, February 2012, update in March 2012    *
+; Register r7 is reserved for interrupt routines as default	      *
+;**********************************************************************
+
+ 	nolist
+	page	255,0
+	opt	MU,S,CC,CEX,MEX,MD
+	list
+
+	nolist
+	include	"SDK\include\dsp\dsp_equ.asm"
+	list
+
+;**********************************************************************
+; The following definition switches between a simulator version and 
+; a real-time version of the program. Set this to '1' if you are 
+; analyzing the program with the simulator, and to '0' if you are 
+; running the program in Chameleon.
+; 
+; The definition is used later in this assembly file to skip or 
+; include sample synchronization, which does not work in the simulator
+; but is essential for correct operation in Chameleon. 
+
+	define	simulator	'0'
+
+;**********************************************************************
+
+
+;**********************************************************************
+; Memory allocations
+;**********************************************************************
+	org	X:$000000	;Denotes the the memory location where the contents of the following lines will be stored 
+;Do your own allocations of X-memory here
+
+
+	
+	org	Y:$000000
+;Do your own allocations of Y-memory here
+
+
+
+;Template related memory allocations
+MasterVolumeTarget:	;Holds the current value of volume pot (log scale $0-$7FFFFF)
+	ds	1	
+MasterVolume:		
+	ds	1		
+CTRL1Value:		;Holds the current value of control pot 1 (lin scale $0-$7FFFFF)
+	ds	1	
+CTRL2Value:		;Holds the current value of control pot 2 (lin scale $0-$7FFFFF)
+	ds	1	
+CTRL3Value:		;Holds the current value of control pot 3 (lin scale $0-$7FFFFF)
+	ds	1
+KeypadState:		;Holds the current state of buttons. Bit values represent the states (1=down, 0=up)
+	ds	1	
+;Bit numbers in "KeypadState" for each button:
+;#8:  Edit
+;#9:  Part up
+;#10: Shift
+;#11; Part down 
+;#16: Group up
+;#17: Page up
+;#18: Group down
+;#19: Page down
+;#20: Param up
+;#21: Value up
+;#22: Param down
+;#23: Value down	
+
+;For debug and simulation
+OutputL:
+	ds	1
+OutputR:
+	ds	1
+	
+
+
+
+
+;**********************************************************************
+; Interrupt vectors
+;**********************************************************************
+	org	P:VecHostCommandDefault
+VecHostCommandUpdateVolume:
+	JSR	>UpdateVolume
+VecHostCommandUpdateCTRL1:
+	JSR	>UpdateCTRL1
+VecHostCommandUpdateCTRL2:
+	JSR	>UpdateCTRL2
+VecHostCommandUpdateCTRL3:
+	JSR	>UpdateCTRL3
+VecHostCommandEncoderUp:
+	JSR	>EncoderUp
+VecHostCommandEncoderDown:
+	JSR	>EncoderDown
+VecHostCommandKeyEvent:
+	JSR	>KeyEvent
+
+;**********************************************************************
+; Program code
+;**********************************************************************
+	org	P:$000100
+
+Start:
+	CLR	A
+	; Enable ESSI0 transmit and receive
+	BSET	#CRB_TE0,X:<<CRB0		; Enable Transmit 0
+	BSET	#CRB_RE,X:<<CRB0		; Enable Receive  0
+	;Interrupt enable
+	ANDI	#<$FC,MR			; Enable interrupts
+	BCLR	#SR_I0,SR			; Unmask interrupts
+	BCLR	#SR_I1,SR
+	BSET	#HCR_HCIE,X:<<HCR  		; Enable Host command interrupt
+	; Initialize Master Volume variables
+	MOVE	A,Y:MasterVolumeTarget
+	MOVE	A,Y:MasterVolume
+	; Channel synchronization
+	if !simulator
+	  BRSET	#SSISR_RFS,X:<<SSISR0,*		; Wait while receiving left frame
+	  BRCLR	#SSISR_RFS,X:<<SSISR0,*		; Wait while receiving right frame
+	endif
+	
+MainLoop:
+
+	; *** Audio input and output are processed here ***
+	; 
+	; The following default code
+	; - copies data from input to output and multiplies with the current gain
+	; - smooths master volume changes
+	;
+	; Note: If you want to work in mono, just use the left channel and copy the output to right channel also
+	
+	
+	;*** LEFT CHANNEL ********************************************************
+	;Input routines for left Ch	
+	if !simulator
+	BRCLR	#SSISR_RDF,X:<<SSISR0,*		; Wait until receive register is full
+	endif
+	MOVEP	X:<<RX0,Y0			; Read new input sample
+
+	; *** Do the processing here for left ch, Y0 holds the sample ****	
+	
+
+
+	
+	;Output routines for left Ch
+	MOVE	Y:MasterVolume,X0		; Current volume value from memory to X0
+	MOVE	Y0,Y:OutputL			; Move the output value to memory for simulator use
+	MPYR	X0,Y0,B				; Multiply the current output sample with the current volume value
+	NOP
+	if !simulator
+	BRCLR	#SSISR_TDE,X:<<SSISR0,*		; Wait for transmit register
+	endif 
+	MOVEP	B,X:<<TX00			; Write new output sample to the DAC
+	
+	;*** RIGHT CHANNEL ********************************************************
+	;Input routines for right Ch
+	if !simulator
+	BRCLR	#SSISR_RDF,X:<<SSISR0,*		; Wait until receive register is full
+	endif
+	MOVEP	X:<<RX0,Y1			; Read new input sample
+
+	; *** Do the processing here for right ch, Y1 holds the sample ****	
+	 
+	  
+	
+	;Output routines for right Ch
+	MOVE	Y:MasterVolume,X1		; Current volume value from memory to X0
+	MOVE	Y1,Y:OutputR			; Move the output value from Y1 to memory for simulator use
+	MPYR	X1,Y1,B				; Scale the input sample according to the volume curve
+	NOP
+	if !simulator
+	BRCLR	#SSISR_TDE,X:<<SSISR0,*		; Wait for transmit register
+	endif 
+	MOVEP	B,X:<<TX00			; Write new output sample to the DAC
+	
+	
+	;** Volume smoothing ******************************************************
+	
+	MOVE	Y:MasterVolumeTarget,B		; target volume value
+	MOVE	Y:MasterVolume,A		; current volume value
+	SUB	A,B				
+	ASR	#10,B,B				; increment
+	ADD	A,B				; current volume value += increment
+	NOP
+	MOVE	B,Y:MasterVolume
+
+	; *** End of audio input and output processing ***
+
+	BRA	MainLoop
+	
+
+;*******************************************	
+;INTERRUPT ROUTINES
+;*******************************************	
+UpdateVolume:
+	BRCLR	#HSR_HRDF,X:<<HSR,*	; Make sure that data is available
+	MOVEP	X:<<HRX,r7		; Read the data to r7	
+	MOVE	r7,Y:MasterVolumeTarget	; Write the data to memory
+	MOVEP	r7,X:<<HTX		; Write the read value back to the MCU	
+	RTI				; Return from interrupt
+UpdateCTRL1:
+	BRCLR	#HSR_HRDF,X:<<HSR,*
+	MOVEP	X:<<HRX,r7
+	MOVE	r7,Y:CTRL1Value
+	MOVEP	r7,X:<<HTX
+	RTI
+UpdateCTRL2:
+	BRCLR	#HSR_HRDF,X:<<HSR,*
+	MOVEP	X:<<HRX,r7
+	MOVE	r7,Y:CTRL2Value
+	MOVEP	r7,X:<<HTX
+	RTI
+UpdateCTRL3:
+	BRCLR	#HSR_HRDF,X:<<HSR,*
+	MOVEP	X:<<HRX,r7
+	MOVE	r7,Y:CTRL3Value
+	MOVEP	r7,X:<<HTX
+	RTI
+KeyEvent:
+	BRCLR	#HSR_HRDF,X:<<HSR,*
+	MOVEP	X:<<HRX,r7
+	MOVE	r7,Y:KeypadState
+	MOVEP	r7,X:<<HTX
+	RTI
+EncoderUp:
+	;Write your encoder up handler here
+
+	RTI
+EncoderDown:
+	;Write your encoder down handler here
+
+	RTI	
+	
+	end	Start
+
---
+++ digimoog/main.c
@@ -0,0 +1,308 @@
+/**********************************************************************
+ * C H A M E L E O N   ColdFire C file                                *
+ **********************************************************************
+ * Project work template for sample-based audio input and output      *
+ * Based on the example dspthru by Soundart                           *
+ * Hannu Pulakka, March 2006, February 2007                           *
+ * Modified by Antti Pakarinen, February, 2012		 	      * 	
+ *	(Panel input and communication routines) 		      *
+ **********************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <rtems.h>
+#include <chameleon.h>
+
+#include "dsp/dsp_code.h"
+
+#define	KEYPAD_EVENT	4
+#define ENCODER_UP	5
+#define ENCODER_DOWN	6
+
+// Required definitions for a Chameleon application
+/**********************************************************************/
+#define WORKSPACE_SIZE	128*1024
+rtems_unsigned32 rtems_workspace_size = WORKSPACE_SIZE;
+rtems_unsigned32 rtems_workspace_start[WORKSPACE_SIZE];
+/**********************************************************************/
+
+// Handles of the panel and the DSP
+int panel, dsp;
+
+
+// This function is called if an unexpected error occurs
+void Error(char *error)
+{
+    TRACE(error);
+    exit(1);
+}
+
+// Show a data word on the LCD display
+void show_data(rtems_signed32 data)
+{
+  char str[16];
+  sprintf(str, "0x%06X", data);
+  panel_out_lcd_print(panel, 1, 0, str);
+}
+
+//** DSP_write ********************************************************************
+// 
+// Description:	Function for writing commands and data to the DSP
+//
+// Parameters:  rtems_unsigned32 event 		
+//			-type of the event that is to be forwarded to the DSP 
+// 		rtems_signed32 data	
+//			-data value to be sent, for example potentiometer position
+//
+// Returns: 	TRUE if successful
+//
+//*********************************************************************************
+
+char DSP_write(rtems_unsigned32 event, rtems_unsigned32 data)
+{
+	
+	switch(event)
+	{
+	case PANEL01_POT_VOLUME:
+	        //First, the data is written to the HI08 port
+	        if (!dsp_write_data(dsp, &data, 1))
+	                Error("ERROR: cannot write data to DSP.\n");
+	        //then, a command is sent to interrupt the DSP and execute the appropriate interrupt routine
+	        if (!dsp_write_command(dsp, DSPP_VecHostCommandUpdateVolume/2, TRUE))
+			Error("ERROR: cannot write command to DSP.\n");
+		return 1;
+	case PANEL01_POT_CTRL1:
+	        if (!dsp_write_data(dsp, &data, 1))
+	                Error("ERROR: cannot write data to DSP.\n");
+	        if (!dsp_write_command(dsp, DSPP_VecHostCommandUpdateCTRL1/2, TRUE))
+			Error("ERROR: cannot write command to DSP.\n");
+		return 1;
+	case PANEL01_POT_CTRL2:
+	        if (!dsp_write_data(dsp, &data, 1))
+	                Error("ERROR: cannot write data to DSP.\n");
+	        if (!dsp_write_command(dsp, DSPP_VecHostCommandUpdateCTRL2/2, TRUE))
+			Error("ERROR: cannot write command to DSP.\n");
+		return 1;
+	case PANEL01_POT_CTRL3:
+	        if (!dsp_write_data(dsp, &data, 1))
+	                Error("ERROR: cannot write data to DSP.\n");
+	        if (!dsp_write_command(dsp, DSPP_VecHostCommandUpdateCTRL3/2, TRUE))
+			Error("ERROR: cannot write command to DSP.\n");
+	  	return 1;
+	case KEYPAD_EVENT:
+		if (!dsp_write_data(dsp, &data, 1))
+	                Error("ERROR: cannot write data to DSP.\n");
+	        if (!dsp_write_command(dsp, DSPP_VecHostCommandKeyEvent/2, TRUE))
+			Error("ERROR: cannot write command to DSP.\n");
+		return 1;
+	case ENCODER_UP:
+	        //No data written, just the command
+	        if (!dsp_write_command(dsp, DSPP_VecHostCommandEncoderUp/2, TRUE))
+			Error("ERROR: cannot write command to DSP.\n");
+	  	return 1;
+	case ENCODER_DOWN:
+		//No data written, just the command
+	        if (!dsp_write_command(dsp, DSPP_VecHostCommandEncoderDown/2, TRUE))
+			Error("ERROR: cannot write command to DSP.\n");
+	  	return 1;
+
+	default:
+	
+		TRACE("\nWrong dsp write source type!\n");
+		return 0;
+	}
+}
+
+
+// Initialization of the panel and the DSP
+void initialize()
+{
+    // Initialize panel and DSP
+    panel = panel_init();
+    if (!panel)
+        Error("ERROR: cannot access the panel.\n");
+
+    dsp = dsp_init(1, dspCode);
+    if (!dsp)
+        Error("ERROR: cannot access the DSP.\n");
+
+    panel_out_lcd_print(panel, 0, 0, "digimoog");
+}
+
+// Panel task: interaction with the Chameleon panel
+static rtems_task panel_task(rtems_task_argument argument)
+{
+    static rtems_signed32 volume_table[128];
+    static rtems_signed32 linear_table[128];
+    rtems_unsigned32  	key_bits;
+    rtems_unsigned32	dummy=0xFFFFFFFF;
+    rtems_unsigned8 	potentiometer;
+    rtems_unsigned8 	encoder;
+    rtems_signed8	increment;
+    char		text[17];
+    
+    rtems_unsigned8 value;
+    int i;
+    float dB;
+
+
+    TRACE("Project work template for sample-based audio input and output\n");
+
+    // Precalculate gain values for different volume settings
+    for (i=0; i<128 ;i++) {
+    	if (i < 27)
+            dB = -90.0 + (float) 40.0*i/27.0;
+        else
+            dB = -50.0 + (float) 50.0*(i-27)/100.0;
+        volume_table[i] = float_to_fix_round(pow(10.0, dB/20.0));
+    }
+    volume_table[0] = 0;
+    
+    // Precalculate a linear table to scale the potentiometer values linearily between 0..~1
+    for (i=1; i<128 ;i++) 
+    {
+    	linear_table[i]=float_to_fix_round((float)i/127.0);	
+    }
+	
+    // Main loop
+    while (TRUE) 
+    {
+
+        // *** Write your panel interaction code here ***
+	//Poll for panel events
+        if (!panel_in_new_event(panel, TRUE))
+        	Error("ERROR: unexpected exit waiting new panel event.\n");
+        if (panel_in_potentiometer(panel, &potentiometer, &value)) 
+        {
+		switch (potentiometer)
+		{
+	             	case PANEL01_POT_VOLUME:	
+	                	DSP_write(PANEL01_POT_VOLUME,volume_table[value]);
+			       	panel_out_lcd_print(panel, 0, 0, "Volume:         ");
+			       	break;
+	                case PANEL01_POT_CTRL1:		
+	                    	DSP_write(PANEL01_POT_CTRL1,linear_table[value]);
+			    	panel_out_lcd_print(panel, 0, 0, "Ctrl1:          ");
+			       	break;
+	                case PANEL01_POT_CTRL2:		
+                	    	DSP_write(PANEL01_POT_CTRL2,linear_table[value]);
+                	    	panel_out_lcd_print(panel, 0, 0, "Ctrl2:          ");
+			       	break;
+		      	case PANEL01_POT_CTRL3:		
+	                    	DSP_write(PANEL01_POT_CTRL3,linear_table[value]);
+	                	panel_out_lcd_print(panel, 0, 0, "Ctrl3:          ");
+			       	break;
+			
+	                default:
+	                	break;
+        	}
+                       
+        }
+        else if (panel_in_keypad(panel, &key_bits))
+	{
+	
+		key_bits>>=8;	//NOTE! shift 8 bits to fit in 24bits in the dsp
+		DSP_write(KEYPAD_EVENT,key_bits);
+		panel_out_lcd_print(panel, 0, 0, "Keypad:         ");
+			
+	}
+	else if (panel_in_encoder(panel, &encoder, &increment))
+	{
+		sprintf(text, "Encoder: %+3d ", increment);
+		if(increment>0)
+			DSP_write(ENCODER_UP,dummy);
+		else
+			DSP_write(ENCODER_DOWN,dummy);	
+		panel_out_lcd_print(panel, 0, 0, text);
+		panel_out_lcd_print(panel, 1, 0, "                ");
+		
+	}
+
+	
+    }
+
+    panel_exit(panel);
+    rtems_task_delete(RTEMS_SELF);
+}
+
+// Create and start the panel task that runs the function panel_task
+rtems_boolean create_panel_task(void)
+{
+    rtems_id task_id;
+    rtems_status_code status;
+
+    status = rtems_task_create(rtems_build_name('T', 'H', 'R', 'U'),
+                               50,
+                               RTEMS_MINIMUM_STACK_SIZE,
+                               RTEMS_DEFAULT_MODES,
+                               RTEMS_DEFAULT_ATTRIBUTES,
+                               &task_id);
+    if (status != RTEMS_SUCCESSFUL) {
+        TRACE("ERROR: cannot create panel_task.\n");
+        return FALSE;
+    }
+
+    status = rtems_task_start(task_id, panel_task, 0);
+    if (status != RTEMS_SUCCESSFUL) {
+        TRACE("ERROR: cannot start panel_task.\n");
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+// Read task: read data from the DSP
+static rtems_task read_task(rtems_task_argument ignored)
+{
+  rtems_signed32 data;
+  rtems_boolean res;
+  
+  while (TRUE) {
+    res = dsp_read_data(dsp, &data, 1);
+    if (res) {
+      data &= 0x00FFFFFF;	//clear the sign extension
+      show_data(data);
+
+      // *** You can implement your own data handling here ***
+
+    }
+  }
+
+  rtems_task_delete(RTEMS_SELF);
+}
+
+// Create and start the read task that runs the function read_task
+rtems_boolean create_read_task(void)
+{
+  rtems_id task_id;
+  rtems_status_code status;
+
+  status = rtems_task_create(
+    rtems_build_name('T', 'R', 'E', 'A'),
+    50,
+    RTEMS_MINIMUM_STACK_SIZE,
+    RTEMS_DEFAULT_MODES,
+    RTEMS_DEFAULT_ATTRIBUTES,
+    &task_id
+  );
+  if (status != RTEMS_SUCCESSFUL) {
+    TRACE("ERROR: cannot create read_task.\n");
+    return FALSE;
+  }
+  status = rtems_task_start(task_id, read_task, 0);
+  if (status != RTEMS_SUCCESSFUL) {
+    TRACE("ERROR: cannot start read_task.\n");
+    return FALSE;
+  }
+  return TRUE;
+}
+
+// The main function that is called when the application is started
+rtems_task rtems_main(rtems_task_argument ignored)
+{
+    initialize();
+    create_panel_task();
+    create_read_task();
+    rtems_task_delete(RTEMS_SELF);
+}
